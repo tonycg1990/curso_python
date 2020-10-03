c4b5ca50239c2cb6dedd0a632b61e652dee4fcae
tonycg1990 <divino_cg_007@hotmail.com>
2020-10-03 00:48:02 -0500

    prueba

---
+++ listas.py
@@ -0,0 +1,14 @@
+# LISTAS: son variables que almacenas una coleccion de datos
+numeros = [1,2,3,4,5,6,7,8,9,10]
+# print (numeros)
+# print (numeros[8])
+# print(numeros[2])
+# for n in numeros:
+#     print(n)
+    
+ciudades=["lima","callao","santa anita","smp","tablada","lurin"]
+# for n in ciudades:
+#     print(n)
+
+# print(ciudades[4],ciudades[1])
+print(ciudades[2:5])
